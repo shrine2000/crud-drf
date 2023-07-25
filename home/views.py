5d42f4c70a4a0570af326667a9270dd912168f53
Shrine Sabu <shrine02sabu@gmail.com>
2023-07-25 22:33:12 +0530

    GET

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def index(request):
+    car = {
+        'suv': ['Innova', 'Endeavour', 'XUV700'],
+        'sedan': ['honda city', 'VW Vitrus', 'Verna'],
+        'hatchback': ['Polo', 'Swift']
+    }
+    return Response(car)
