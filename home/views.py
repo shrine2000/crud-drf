4373f272c723747e1fa9b39333d4aadd0bd1a3db
Shrine Sabu <shrine02sabu@gmail.com>
2023-07-26 12:47:20 +0530

    reformat

--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Car
 from .serializers import CarSerializer
+
+
 @api_view(['GET'])
 def index(request):
     car = {
@@ -11,11 +13,12 @@ def index(request):
     }
     return Response(car)
 
+
 @api_view(['GET', 'POST'])
 def vehicle(request):
     if request.method == 'GET':
-        cars = Car.objects.all() # returns query set
-        serializer = CarSerializer(cars, many=True) # many=True because we are using taking more than one object
+        cars = Car.objects.all()  # returns query set
+        serializer = CarSerializer(cars, many=True)  # many=True because we are using taking more than one object
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = CarSerializer(data=request.data)
@@ -23,4 +26,3 @@ def vehicle(request):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
-
