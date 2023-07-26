46d35834e2366fc5c3d30c0477bf653cbabbf518
Shrine Sabu <shrine02sabu@gmail.com>
2023-07-26 12:37:59 +0530

    POST

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
-from home.views import index
+from home.views import index, vehicle
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path('index/', index),
+    path('vehicle/', vehicle)
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-
+from .models import Car
+from .serializers import CarSerializer
 @api_view(['GET'])
 def index(request):
     car = {
@@ -10,3 +10,17 @@ def index(request):
         'hatchback': ['Polo', 'Swift']
     }
     return Response(car)
+
+@api_view(['GET', 'POST'])
+def vehicle(request):
+    if request.method == 'GET':
+        cars = Car.objects.all() # returns query set
+        serializer = CarSerializer(cars, many=True) # many=True because we are using taking more than one object
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CarSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
