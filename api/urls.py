27872de42d3a104c5cbee8a6ed32ec2a8602567c
Shrine Sabu <shrine02sabu@gmail.com>
2023-07-26 17:34:07 +0530

    added PUT, PATCH

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,10 @@
-from home.views import index, vehicle
+from home.views import index, vehicle, vehicle_detail
 from django.contrib import admin
 from django.urls import path
+from home import views
 
 urlpatterns = [
     path('index/', index),
-    path('vehicle/', vehicle)
+    path('vehicle/', vehicle),
+    path('vehicle/<int:pk>/', vehicle_detail),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Car
 from .serializers import CarSerializer
 
 
+# https://www.bezkoder.com/django-rest-api/
 @api_view(['GET'])
 def index(request):
     car = {
@@ -24,5 +26,31 @@ def vehicle(request):
         serializer = CarSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['PUT', 'PATCH'])
+def vehicle_detail(request, pk=None):
+    if request.method == 'PUT':
+        try:
+            car = Car.objects.get(pk=pk)
+        except Car.DoesNotExist:
+            return Response({'error': 'Car not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = CarSerializer(car, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'PATCH':
+        if pk is not None:
+            car = Car.objects.get(pk=pk)
+            serializer = CarSerializer(car, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"detail": "Not found"}, status=status.HTTP_404_NOT_FOUND)
