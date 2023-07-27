8988ac5d5826d5b99575fdd3b8c17fb13691840c
Shrine Sabu <shrine02sabu@gmail.com>
2023-07-27 11:38:59 +0530

    year validation

--- home/serializers.py
+++ home/serializers.py
@@ -8,3 +8,9 @@ class CarSerializer(serializers.ModelSerializer):
         fields = '__all__'  # Include all fields of the Car model
         # fields = ['year', 'make']  # Include only 'year' and 'make' fields from the Car model
         # use exclude = ['make'] to remove certain fields when using '__all__'
+
+    def validate_year(self, value):
+        if value < 1900 or value > 2025:
+            raise serializers.ValidationError("Year must be between 1900 and 2025")
+        return value
+
