5d87d4fd1fa6ee9dc82795f890f144705d7b759e
Shrine Sabu <shrine02sabu@gmail.com>
2023-07-28 11:15:56 +0530

    added bike model

--- home/models.py
+++ home/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Car(models.Model):
     make = models.CharField(max_length=100)
     year = models.IntegerField()
+
+
+class Bike(models.Model):
+    make = models.CharField(max_length=100)
+    year = models.IntegerField()
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='bikes')
